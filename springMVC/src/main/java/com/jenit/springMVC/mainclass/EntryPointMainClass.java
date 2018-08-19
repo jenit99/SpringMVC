417eec4d1fa60685d0f3c6a4c9a3667405e99818
Jenit Patel <JXP08740@ucmo.edu>
2018-08-19 13:56:43 -0400

    This is my first commit

---
+++ springMVC/src/main/java/com/jenit/springMVC/mainclass/EntryPointMainClass.java
@@ -0,0 +1,11 @@
+package com.jenit.springMVC.mainclass;
+
+public class EntryPointMainClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World ! Right ? ");
+
+	}
+
+}
